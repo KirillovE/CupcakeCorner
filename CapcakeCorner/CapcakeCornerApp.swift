833124dd32804873c6612e6f7072ed6f636b0d3e
Eugène <Eugene.Kiriloff@iCloud.com>
2025-03-04 18:31:15 +0100

    Initial Commit

---
+++ CapcakeCorner/CapcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapcakeCornerApp.swift
+//  CapcakeCorner
+//
+//  Created by Eugène Kiriloff on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CapcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
